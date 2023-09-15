66bc41e1703c3491a0afae3cb6aeb23d49c49a7c
a-azevedo <aelmajan.azevedo@gmail.com>
2023-09-15 11:06:51 -0300

    Created method orderByAge

--- src/ordering/Person.java
+++ src/ordering/Person.java
@@ -1,10 +1,52 @@
 package ordering;
 
-public class Pessoa {
+public class Person implements Comparable<Person>{
 
     private String name;
     private int age;
-    private int height;
+    private double height;
 
-    public 
+    public Person(String name, int age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(age, o.getAge());
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+
 }
--- src/ordering/PersonOrdering.java
+++ src/ordering/PersonOrdering.java
@@ -1,2 +1,36 @@
-package ordering;public class PersonOrdering {
+package ordering;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PersonOrdering {
+
+    List<Person> persons;
+
+    public PersonOrdering(){
+        this.persons = new ArrayList<>();
+    }
+
+    public void addPerson(String name, int age, double height){
+        persons.add(new Person(name, age, height));
+    }
+
+    public List<Person> orderByAge(){
+        List<Person> personsByAge = new ArrayList<>(persons);
+        Collections.sort(personsByAge);
+        return personsByAge;
+    }
+
+    public static void main(String[] args) {
+
+        PersonOrdering po = new PersonOrdering();
+
+        po.addPerson("Aelmajan", 35, 1.76d);
+        po.addPerson("Luffy", 20, 1.65d);
+        po.addPerson("Laerte", 47, 1.74d);
+
+        System.out.println(po.persons);
+        System.out.println(po.orderByAge());
+    }
 }
